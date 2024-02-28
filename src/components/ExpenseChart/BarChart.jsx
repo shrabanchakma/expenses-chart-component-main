03736e5bbcca3a64629e8fedffa803fa22342fe9
Shraban Chakma <shrabanchakma328@gmail.com>
2024-02-29 01:40:47 +0600

    shows pointer hovering on a bar

--- src/components/ExpenseChart/BarChart.jsx
+++ src/components/ExpenseChart/BarChart.jsx
@@ -1,16 +1,26 @@
 import { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
-import {
-  Chart,
-  CategoryScale,
-  LinearScale,
-  BarController,
-  BarElement,
-} from "chart.js";
-Chart.register(CategoryScale, LinearScale, BarController, BarElement);
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
 
 const BarChart = ({ barChartData = [] }) => {
   //   console.log(barChartData);
+
+  const barBox = {
+    id: "barBox",
+    afterDatasetsDraw(chart, args, pluginOptions) {
+      const {
+        ctx,
+        chartArea: { top, bottom },
+        data,
+        scales: { x, y },
+      } = chart;
+      ctx.save();
+
+      //   console.log(chart.getDatasetMeta(0).data[0].active);
+    },
+  };
+
   const labels = barChartData.map((dataItem) => dataItem.day);
   const chartData = barChartData.map((dataItem) => dataItem.amount);
   const data = {
@@ -31,13 +41,24 @@ const BarChart = ({ barChartData = [] }) => {
         beginAtZero: true,
       },
     },
-    plugins: {
-      legend: {
-        display: true,
-      },
+    plugins: [barBox],
+    onHover: (e, chartElement) => {
+      if (chartElement.length === 1) {
+        console.log(e.native.target);
+        e.native.target.style.cursor = "pointer";
+      } else {
+        e.native.target.style.cursor = "default";
+      }
     },
   };
-  return <Bar data={data} options={options} className="bg-white" />;
+  return (
+    <Bar
+      data={data}
+      options={options}
+      plugins={[barBox]}
+      className="bg-white"
+    />
+  );
 };
 
 export default BarChart;

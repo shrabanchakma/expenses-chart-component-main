eb67ddeba83336ab34a985d38d0ddeb9fd004a81
Shraban Chakma <shrabanchakma328@gmail.com>
2024-02-29 20:56:48 +0600

    added total spending of the month

--- src/components/ExpenseChart/BarChart.jsx
+++ src/components/ExpenseChart/BarChart.jsx
@@ -1,26 +1,8 @@
-import { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
 import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
 
 const BarChart = ({ barChartData = [] }) => {
-  //   console.log(barChartData);
-
-  const barBox = {
-    id: "barBox",
-    afterDatasetsDraw(chart, args, pluginOptions) {
-      const {
-        ctx,
-        chartArea: { top, bottom },
-        data,
-        scales: { x, y },
-      } = chart;
-      ctx.save();
-
-      //   console.log(chart.getDatasetMeta(0).data[0].active);
-    },
-  };
-
   const labels = barChartData.map((dataItem) => dataItem.day);
   const chartData = barChartData.map((dataItem) => dataItem.amount);
   const data = {
@@ -36,6 +18,11 @@ const BarChart = ({ barChartData = [] }) => {
 
   const options = {
     maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
     scales: {
       x: {
         border: {
@@ -47,21 +34,21 @@ const BarChart = ({ barChartData = [] }) => {
         },
       },
       y: {
+        border: {
+          display: false,
+        },
         beginAtZero: true,
         ticks: {
           display: false,
         },
         grid: {
-          display: false,
           drawOnChartArea: false,
           drawTicks: false,
         },
       },
     },
-    plugins: [barBox],
     onHover: (e, chartElement) => {
       if (chartElement.length === 1) {
-        console.log(e);
         e.native.target.style.cursor = "pointer";
       } else {
         e.native.target.style.cursor = "default";
@@ -69,12 +56,31 @@ const BarChart = ({ barChartData = [] }) => {
     },
   };
   return (
-    <Bar
-      data={data}
-      options={options}
-      plugins={[barBox]}
-      className="bg-white rounded-3xl"
-    />
+    <div className="relative h-[50vw] md:h-[10vw] border rounded-t-3xl bg-[#FFFCF7]">
+      <h1 className="px-7 py-7 text-4xl font-bold text-[#422D1F]">
+        Spending - Last 7 days
+      </h1>
+      <Bar data={data} options={options} className="bg-[#FFFCF7] px-6" />
+      <div className="text-3xl bg-[#FFFCF7] px-6">
+        <div className="divider m-0 pt-5 divider-neutral"></div>
+        <div className="flex items-center justify-between py-10">
+          {/* total  spending*/}
+          <div>
+            <h3 className="text-lg font-normal text-[#8E8882]">
+              Total this month
+            </h3>
+            <p className="text-5xl font-bold">$478.33</p>
+          </div>
+          {/* percentage */}
+          <div className="text-end">
+            <h3 className="text-lg font-bold">+2.4%</h3>
+            <p className="text-[#8E8882] text-lg font-normal">
+              from last month
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/ExpenseChart/ExpenseChart.jsx
+++ src/components/ExpenseChart/ExpenseChart.jsx
@@ -18,24 +18,22 @@ const ExpenseChart = () => {
   console.log(barChartData);
 
   return (
-    <div className="w-[100vw] min-h-[100vh] flex justify-center items-center bg-green-500">
+    <div className="w-[100vw] min-h-[100vh] flex justify-center items-center bg-[#F7E9DC]">
       <div className="w-[34rem]">
         {/* todo: heading */}
-        <div className="flex justify-between py-[1.7rem]  px-[2rem] rounded-2xl bg-[#EC755D] mb-3 ">
+        <div className="flex justify-between py-[1rem]  px-[1rem] rounded-2xl bg-[#EC755D] mb-3 ">
           <div className="text-white">
-            <h1 className="font-medium text-lg mb-2">My Balance</h1>
-            <p className="font-bold text-3xl">$921.48</p>
+            <h1 className="font-bold text-lg">My Balance</h1>
+            <p className="font-bold text-3xl m-0">$921.48</p>
           </div>
           <img src={logo} alt={logo} />
         </div>
         {/* todo: chart */}
-        <div className="relative h-[50vw] md:h-[10vw] border rounded-3xl">
-          {barChartData && barChartData.length ? (
-            <BarChart barChartData={barChartData} />
-          ) : (
-            <p>text not found</p>
-          )}
-        </div>
+        {barChartData && barChartData.length ? (
+          <BarChart barChartData={barChartData} />
+        ) : (
+          <p>Chart Not Found</p>
+        )}
       </div>
     </div>
   );
